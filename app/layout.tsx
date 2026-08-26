19fea3167de5dc81bf1c70c8c4f3ad79db6d49a1
Arnav Verma <arnav.verma.feb.2009@gmail.com>
2026-08-26 23:12:00 +0530

    Initial Site commit + Navbar functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-mist-950 font-sans">{children}</body>
     </html>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { HoverBorderGradient } from "./ui/hover-button-gradient"
+import { useState } from "react"
+import { MenuIcon, X } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const navbar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className=" w-screen flex lg:justify-center border-b border-mist-900 h-full">
+
+    <div className="flex xl:gap-52 bg-mist-950/50 xl:max-w-5xl lg:max-w-3xl lg:gap-28 gap-4  w-full h-20  font-bold text-2xl items-center justify-between backdrop-blur-lg  px-10 border-mist-900">
+     <Link href="/"> <div>
+        
+        <Image
+          src="/logo.svg"
+          alt="SoxFindr Logo"
+          width={40}
+          height={40}
+        />
+      </div>
+      </Link>
+      <div className=" gap-4 hidden lg:flex">
+        <Link href="/">
+          <Button variant="ghost" className={"text-accent hover:bg-transparent text-[16px] hover:text-accent/80"} size="lg">
+            Home
+          </Button>
+        </Link>   <Link href="/societies">
+          <Button variant="ghost" className={"text-accent hover:bg-transparent text-[16px] hover:text-accent/80"} size="lg">
+            Societies
+          </Button>
+        </Link>   <Link href="/guide">
+          <Button variant="ghost" className={"text-accent hover:bg-transparent text-[16px] hover:text-accent/80"} size="lg">
+            Guide
+          </Button>
+        </Link>   <Link href="/faqs">
+          <Button variant="ghost" className={"text-accent hover:bg-transparent text-[16px] hover:text-accent/80"} size="lg">
+            FAQs
+          </Button>
+        </Link>
+      </div> <div className="flex gap-10">
+      <Link href="/">
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="bg-white hidden lg:block text-sm  font-bold hover:bg-mist-200 hover:text-mist-800 transition duration-1000 text-mist-950  space-x-2"
+            clockwise={false}
+          >
+            <span>Register</span>
+          </HoverBorderGradient>
+        <MenuIcon
+            onClick={() => setIsExpanded(true)}
+            className="h-8 w-8 text-white block lg:hidden"
+          />
+        </Link>
+      </div>
+    </div>
+     <div
+        className={cn(
+          "flex xl:hidden bg-transparent fixed z-10",
+          // isExpanded && "top-0 transition duration-500"
+        )}
+      >
+        <div
+          className={cn(
+            "hidden h-screen fixed w-screen  transition duration-500 z-10 bg-mist-900/70 backdrop-blur-xl text-white",
+            isExpanded && "flex flex-col absolute "
+          )}
+        >
+          <div className="h-20   w-full flex justify-between items-center p-7">
+            <Link href={"/"}>
+              <Image src={"/logo.svg"} height={40} width={40} alt="logo" />{" "}
+            </Link>
+
+            <X
+              onClick={() => setIsExpanded(false)}
+              className="h-10 w-10 text-white fill"
+            />
+          </div>
+          <div
+            className={cn(
+              "transition duration-500 px-7 justify-center  gap-y-4 text-7xl h-full flex flex-col",
+              isExpanded && "flex flex-col"
+            )}
+          >
+            <Link href={"/"}>Home</Link>
+            <Link href={"/societies"}>Societies</Link>
+            <Link href={"/guide"}>Guide</Link>
+            <Link href={"/faqs"}>FAQs</Link>
+          </div>
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default navbar
