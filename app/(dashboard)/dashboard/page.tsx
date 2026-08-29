8af793461ecc654d30e4040bbc8d4d5c827325da
Arnav Verma <arnav.verma.feb.2009@gmail.com>
2026-08-29 22:48:20 +0530

    added user dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import DashNavbar from '@/components/dashboard/dash-navbar'
+import UserBoards from '@/components/dashboard/user-boards'
 import { getOrCreateUser } from '@/db/user'
 import { getUserApplications } from '@/db/userApplications'
 import { redirect } from 'next/navigation'
@@ -6,8 +7,6 @@ import { redirect } from 'next/navigation'
 const DashboardPage = async () => {
   const user = await getOrCreateUser()
   if (user === null) return redirect('/')
-  const applications = await getUserApplications(user.id)
-
   return (
     <div className='text-accent '>
       <DashNavbar />
@@ -15,11 +14,8 @@ const DashboardPage = async () => {
       {user?.role === "SUPER ADMIN" ? "super sigma" : 
       <>
         <div>
-          {applications.map((app) => (
-            <div key={app.id}>
-              {app.whyYou}
-            </div>
-          ))}
+          
+          <UserBoards />
         </div>
       </>
       }
